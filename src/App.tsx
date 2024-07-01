98a3da33dd42000494ecc02c722c2d724b241eb1
damian <damianjnc@gmail.com>
2024-07-01 22:42:29 +0100

    test 4

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const searchClient = algoliasearch(
 const future = { preserveSharedStateOnUnmount: true };
 
 export function App() {
-  console.log('App2');
+  console.log('App23');
 
   return (
     <div>
